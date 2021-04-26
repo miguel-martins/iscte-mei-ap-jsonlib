397d2d857663ec3773e540ab14a39ad4069e08cb
mfnms-iscteiul <miguelmartins737@gmail.com>
2021-04-26 19:47:34 +0100

    JSON Reflection Tests

--- src/test/kotlin/tests/JSONReflectionTest.kt
+++ src/test/kotlin/tests/JSONReflectionTest.kt
@@ -3,7 +3,9 @@ package tests
 import generator.JSONExclude
 import generator.JSONGenerator
 import generator.JSONIdentifier
+import org.junit.jupiter.api.Test
 import visitor.JSONVisitor
+import kotlin.test.assertEquals
 
 //class JSONReflectionTest {}
 
@@ -12,17 +14,25 @@ enum class CustomType {
     CustomType1, CustomType2, CustomType3
 }
 
-fun main() {
+val json = JSONClass(null,"String Value", 6, false, CustomType.CustomType1)
+val json2 = JSONClass(null,"String Value", 6, false, CustomType.CustomType1, json)
+val jsonGenerator = JSONGenerator()
+val jsonSerializer = JSONVisitor()
 
-    val json = JSONClass(null,"String Value", 6, false, CustomType.CustomType1)
-    val json2 = JSONClass(null,"String Value", 6, false, CustomType.CustomType1, json)
-    val jsonGenerator = JSONGenerator()
-    val jsonSerializer = JSONVisitor()
-    jsonGenerator.buildFrom(json2).accept(jsonSerializer)
-    println(jsonSerializer.jsonString)
+val customList = mutableListOf(1,null,true, "hello", json)
+val jsonSerializer2 = JSONVisitor()
 
-    val customList = mutableListOf<Any>(1,true, "hello", json)
-    val jsonSerializer2 = JSONVisitor()
-    jsonGenerator.buildFrom(customList).accept(jsonSerializer2)
-    println(jsonSerializer2.jsonString)
+class JSONReflectionTest {
+
+    @Test
+    fun testObjectGeneration(){
+        jsonGenerator.buildFrom(json2).accept(jsonSerializer)
+        assertEquals("{ \"booleanValue\": false, \"child\": { \"booleanValue\": false, \"child\": null, \"numberValue\": 6, \"stringId\": \"String Value\", \"type\": \"CustomType1\" }, \"numberValue\": 6, \"stringId\": \"String Value\", \"type\": \"CustomType1\" }", jsonSerializer.jsonString)
+    }
+
+    @Test
+    fun testArrayGeneration(){
+        jsonGenerator.buildFrom(customList).accept(jsonSerializer2)
+        assertEquals("[ 1, null, true, \"hello\", { \"booleanValue\": false, \"child\": null, \"numberValue\": 6, \"stringId\": \"String Value\", \"type\": \"CustomType1\" } ]", jsonSerializer2.jsonString)
+    }
 }
